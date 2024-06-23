b9cdbf14cf73595ab697b8e1c63d444e0c105046
elyte5star <checkuti@gmail.com>
2024-06-23 02:00:18 +0200

    working on jwt

--- common/middleware/auth.go
+++ common/middleware/auth.go
@@ -4,30 +4,21 @@ import (
 	"github.com/api/repository/response"
 	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
-	"github.com/google/uuid"
+	
 )
 
-type UserPrincipal struct {
-	Userid                  uuid.UUID `json:"userid"`
-	Username                string    `json:"username"`
-	Password                []byte    `json:"password"`
-	Email                   string    `json:"email"`
-	IsEnabled               bool      `json:"isEnabled "`
-	IsAccountNonLocked      bool      `json:"isAccountNonLocked"`
-	IsCredentialsNonExpired bool      `json:"isCredentialsNonExpired"`
-	IsAdmin                 bool      `json:"isAdmin"`
-}
+
 
 // Middleware JWT function
-func (p *UserPrincipal) NewAuthMiddleware(secret string) fiber.Handler {
+func NewAuthMiddleware(secret string) fiber.Handler {
 	return jwtware.New(jwtware.Config{
 		SigningKey:   jwtware.SigningKey{Key: []byte(secret)},
 		ContextKey:   "jwt",
-		ErrorHandler: p.abortAuthenticationFailed,
+		ErrorHandler: abortAuthenticationFailed,
 	})
 }
 
-func (p *UserPrincipal) abortAuthenticationFailed(c *fiber.Ctx, err error) error {
+func abortAuthenticationFailed(c *fiber.Ctx, err error) error {
 	newErr := response.NewErrorResponse()
 	newErr.Message = err.Error()
 	newErr.Code = fiber.StatusUnauthorized

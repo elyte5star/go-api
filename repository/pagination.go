787ad0074c488182a84913fe782b94398bf62a1f
OGAGA UTI <elyte@OGAGAs-MacBook-Pro.local>
2024-07-29 22:14:16 +0200

    working on paging

--- repository/pagination.go
+++ repository/pagination.go
@@ -1,19 +1,34 @@
 package repository
 
-import "github.com/jmoiron/sqlx"
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
 
 type PaginationQueries struct {
 	*sqlx.DB
 }
 
 type Pagination struct {
-	Next          int
-	Previous      int
-	RecordPerPage int
-	CurrentPage   int
-	TotalPage     int
+	Next             int  `json:"next"`
+	Previous         int  `json:"previous"`
+	RecordPerPage    int  `json:"recordPerPage"`
+	CurrentPage      int  `json:"currentPage"`
+	TotalPages       int  `json:"totalPages"`
+	NumberOfElements int  `json:"numberOfElements"`
+	Number           int  `json:"number"`
+	TotalElements    int  `json:"totalElements"`
+	Empty            bool `json:"empty"`
+	Last             bool `json:"last"`
+	First            bool `json:"first"`
 }
 
 func (q *PaginationQueries) Pageable(table string, page int) *Pagination {
+	const LIMIT = 12
+	var rowCount int
+	query := fmt.Sprintf("SELECT COUNT(*) FROM %s", table)
+	_ = q.QueryRow(query).Scan(&rowCount)
+	total := (rowCount / LIMIT)
 	return &Pagination{}
 }
